560135da88b4efdda62845cd83998453bb36af72
Viacheslav991 <viacheslavd91@gmail.com>
2024-11-07 23:48:20 +0300

    Add files via upload

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,15 +1,14 @@
 import configuration
 import requests
 import data
+import Order_information_on_TrackNumber_test as Order
 def create_new_order(body):
     return requests.post(configuration.URL_SERVICE + configuration.MAKING_NEW_ORDER, #Функция для создания нового заказа
                          json=body,
                          headers=data.headers)
-
 def get_order_status():
-    get_order = create_new_order(data.order_body)
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_STATUS,#Функция запроса заказа по трек номеру
-                        params=get_order.text)
+                        params=Order.get_order.text)
 
 
 

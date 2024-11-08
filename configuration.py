ae589e2a5c9cdd56906cce016b81a6f46e970aa8
Viacheslav991 <viacheslavd91@gmail.com>
2024-11-08 14:14:56 +0300

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://71c0d202-a5fa-47c9-9011-259044b38b8a.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://2b6743fa-0c00-4de2-bcf7-bfa554110331.serverhub.praktikum-services.ru"
 
 MAKING_NEW_ORDER = "/api/v1/orders" #Переменные API для создания нового заказа и получения запроса о сделанных заказах
 GET_ORDER_STATUS = "/api/v1/orders/"  #Не менять
